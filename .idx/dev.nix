3ead3feb082ef2268446cc14989c9c962e51d25d
bouajila.hamza.ai <bouajila.hamza.ai@gmail.com>
2025-11-01 10:25:34 +0000

    add docker  file

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,6 +7,8 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
+    pkgs.docker
+    pkgs.docker-client
     pkgs.apt
     pkgs.python311
     pkgs.python311Packages.pip
